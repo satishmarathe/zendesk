88ce7c5308bd6a17cda2f3906fe1ed7fd14f811d
Satish.Marathe <marathes_pune@yahoo.com>
2021-08-09 08:41:41 +1000

    refactoring

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is the minimum versions of software required to run this code:
 Java 1.8
 Apache Maven 3.2.5 
 
+Other dependencies are specified in pom file and will be made available when we compile and build the code.
+
 # Instructions
 To compile the code simply run the command at root of project:
 	mvn clean install
@@ -14,80 +16,45 @@ This should successfully compile the code and build a jar : zendesk-1.0.0-RELEAS
 
 Code coverage can be seen via index.html that gets built within target folder.
 
-This program expects commands to be fed from a file 
+This program expects commands to be entered at command prompt 
 
-To run the code run following command where the compiled jar is located :
-Compiled jar file name = zendesk-1.0.0-RELEASE.jar
-Also need to include the two logging api jars are on classpath : log4j-api-2.14.0.jar and  log4j-core-2.14.0.jar
+To run the code navigate to 'target' directory  : ( this is where Compiled jar file : zendesk-1.0.0-RELEASE.jar will be present )
+We also need to include dependant jar files on classpath which are : 
+log4j-api-2.14.0.jar 
+log4j-core-2.14.0.jar
+commons-io-2.11.0.jar
+gson-2.8.7.jar
+commons-lang3-3.12.0.jar
 
 Example:
-java -cp .;zendesk-1.0.0-RELEASE.jar;C:\construction\maven_repository\org\apache\logging\log4j\log4j-api\2.14.0\log4j-api-2.14.0.jar;C:\construction\maven_repository\org\apache\logging\log4j\log4j-core\2.14.0\log4j-core-2.14.0.jar;C:\construction\maven_repository\commons-io\commons-io\2.11.0\commons-io-2.11.0.jar;C:\construction\maven_repository\com\google\code\gson\gson\2.8.7\gson-2.8.7.jar;C:\construction\maven_repository\org\apache\commons\commons-lang3\3.12.0\commons-lang3-3.12.0.jar  com.zensearch.ZendeskSolution
+java -cp .;zendesk-1.0.0-RELEASE.jar;C:\maven_repository\log4j-api-2.14.0.jar;C:\\maven_repository\log4j-core-2.14.0.jar;C:\maven_repository\commons-io-2.11.0.jar;C:\maven_repository\gson-2.8.7.jar;C:\maven_repository\commons-lang3-3.12.0.jar  com.zensearch.ZendeskSolution
 
 
 
 # Assumptions
+1 Code will exit once a search is performed 
+2 Code will not exit and start from the beginning for any invalid options entered ( other than quit ) 
 
 # Scenarios to Unit Test / Integration Test
 ## Search by User attributes having tickets associated to user
-### 1 Search by 'created_at'
-### 2 Search by 'name'
-### 3 Search by 'verified'
-### 4 Search by '_id'
+1 Search by 'created_at'
+2 Search by 'name'
+3 Search by 'verified'
+4 Search by '_id'
 
 ## Search by User attributes having NO tickets associated to user
-### 1 Search by 'created_at'
-### 2 Search by 'name'
-### 3 Search by 'verified'
-### 4 Search by '_id'
+1 Search by 'created_at'
+2 Search by 'name'
+3 Search by 'verified'
+4 Search by '_id'
 
 ## Search by Ticket attributes having users assigned to ticket
-### 1 Search by '_id'
-### 2 Search by 'created_at'
-### 3 Search by 'type'
-### 4 Search by 'subject'
-### 5 Search by 'assignee_id'
-### 6 Search by 'tags'
-
-# Notes
-## 1
-To read json data have used GSON Library
-To handle file reading we have used apache commons 
-
-# Learnings
-## 1 Inverted Index
-https://medium.com/@igorkopanev/a-brief-explanation-of-the-inverted-index-f082993f8605
-https://dev.to/im_bhatman/introduction-to-inverted-indexes-l04
-
-## 2 Large file handling
-https://dev.to/franzwong/process-large-json-with-limited-memory-12kb
-https://newbedev.com/java-best-approach-to-parse-huge-extra-large-json-file
-
-## 3 Scanner
-Using the method next is giving wierd results for subsequent scanned inputs 
-So have used 'nextLine' to capture user input
-https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
-
-# TODO
-
-###2
-add loggers
-
-###4
-Junits
+1 Search by '_id'
+2 Search by 'created_at'
+3 Search by 'type'
+4 Search by 'subject'
+5 Search by 'assignee_id'
+6 Search by 'tags'
 
 
-# DONE
-###1
-Need to prevent wrong entry while entering key and value
 
-###2
-Show results for searchable fields 
-
-###3
-No results found implementation 
-
-###4
-remove extra and unnecessary sys outs
-
-###5 
-Search by tag implemented
--- src/main/java/com/zensearch/ZendeskSolution.java
+++ src/main/java/com/zensearch/ZendeskSolution.java
@@ -1,19 +1,13 @@
 package com.zensearch;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.zensearch.pojo.Ticket;
 import com.zensearch.pojo.User;
 import com.zensearch.repo.TicketRepoImpl;
@@ -26,81 +20,37 @@ import com.zensearch.service.ServiceImpl;
 public class ZendeskSolution {
 
 	private static final Logger logger = LogManager.getLogger(ZendeskSolution.class);
-
+	
+	private static final String MENU_ONE = "1";
+	private static final String MENU_TWO = "2";
+	private static final String MENU_QUIT = "quit";
+	
 	enum UserSearchableFields { _id, name, created_at, verified };
 	enum TicketSearchableFields { _id, type, created_at, subject , assignee_id , tags };
 	
-
-
+	
 	public static void main(String[] args) {
-		logger.info("start");
-		
-		
-		
-		/** First we need to read both files i.e. tickets and users **/
-		/** Second Take user inputs and handle them **/
-		/** In case of an empty file or file not found - log errors to log and let user know something is wrong **/
-		/** Handle invalid commands by letting them know list of allowed / valid commands **/
-		/** do not exit unless user has explicitly hit quit **/
-		InputStreamReader ticketReader = null;
-		InputStreamReader userReader = null;
-		List<Ticket> ticketList = null;
-		List<User> userList = null;
-		try {			
-			InputStream userInputStream = ZendeskSolution.class.getClassLoader().getResourceAsStream("users.json");
-			InputStream ticketInputStream = ZendeskSolution.class.getClassLoader().getResourceAsStream("tickets.json");
-
-			try {
-
-				ticketReader = new InputStreamReader(ticketInputStream,"UTF-8");
-				userReader = new InputStreamReader(userInputStream,"UTF-8");
-
-				ticketList = new Gson().fromJson(ticketReader, new TypeToken<List<Ticket>>() {}.getType());
-				userList = new Gson().fromJson(userReader, new TypeToken<List<User>>() {}.getType());
 				
-				
-
-				ticketReader.close();
-				userReader.close();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				//IOUtils.closeQuietly(userInputStream);
-				IOUtils.closeQuietly(ticketInputStream);
-				IOUtils.closeQuietly(userInputStream);
-			}
-
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		/** if we have the list of tickets lets organise the data to help make search scale **/
-
-		
-
-		
-		
-		
-
-
-
-		
 		/** perform search based on user input **/
 		String userInput;
 
 		Scanner scan = new Scanner(System.in).useDelimiter("\n");
 		;
 		do {
-			System.out.println("welcome to zendesk type quit to exit Enter to continue");
+			System.out.println("Welcome to Zendesk Search");
+			System.out.println("Type 'quit' to exit at any time,Press 'Enter' to continue");
 			userInput = scan.nextLine();
 			if(userInput.trim().equals("")) {
-				System.out.println("select 1 to search 2 to view list of searchable fields and quit to exit");
+				System.out.println("Select search options:");
+				System.out.println( "* Press 1 to search Zendesk");
+				System.out.println( "* Press 2 to view a list of searchable fields");
+				System.out.println( "* Type 'quit' to exit");
+				System.out.println();
 				userInput = scan.nextLine();
-				if(userInput.trim().equals("1")) {
-					System.out.println(" select 1 to search users and 2 to search tickets");
+				if(userInput.trim().equals(MENU_ONE)) {
+					System.out.println("Select 1) Users or 2) Tickets");
 					userInput = scan.nextLine();
-					if(userInput.trim().equals("1")) {
+					if(userInput.trim().equals(MENU_ONE)) {
 						System.out.println("Enter search term");
 						String userKey = scan.nextLine();
 						System.out.println("Enter search value");
@@ -113,7 +63,7 @@ public class ZendeskSolution {
 							System.out.println("Searching users for " + userKey + " with a value of " + userValue);
 							List <User> userResults = new ArrayList<User>();
 							ServiceIfc service = new ServiceImpl(new TicketRepoImpl(),new UserRepoImpl());
-							if(userKey.trim().equals("_id")) {
+							if(userKey.trim().equals("_id")) {								
 								/** this will be a simpler search without having to do convoluted extraction so handle separately **/
 								/**
 								userResults = getUserDisplayResults(userKey,userValue,masterTicketMap,
@@ -136,11 +86,13 @@ public class ZendeskSolution {
 							}else {
 								System.out.println(userResults);
 							}
+							break;
 						}else {
 							System.out.println("invalid search criteria for User search ! ");
+							System.out.println(" ");
 						}
-						break;
-					}else if(userInput.trim().equals("2")) {
+						
+					}else if(userInput.trim().equals(MENU_TWO)) {
 						System.out.println("Enter search term");
 						String ticketKey = scan.nextLine();
 						System.out.println("Enter search value");
@@ -176,15 +128,17 @@ public class ZendeskSolution {
 							}else {
 								System.out.println(ticketResults);
 							}	
+							break;
 						}else {
 							System.out.println("invalid search criteria for Ticket search! ");
-						}						
+							System.out.println(" ");
+						}									
+					}else if(userInput.trim().equals(MENU_QUIT)){
 						break;
 					}else {
 						System.out.println("Invalid selection , valid options to search by user is 1 and by ticket 2 .\n");
-						break;
 					}
-				}else if(userInput.trim().equals("2")) {
+				}else if(userInput.trim().equals(MENU_TWO)) {
 					System.out.println("----------------------------");
 					System.out.println("Search users with");
 					System.out.println(UserSearchableFields._id);
@@ -199,20 +153,19 @@ public class ZendeskSolution {
 					System.out.println(TicketSearchableFields.subject);
 					System.out.println(TicketSearchableFields.assignee_id);
 					System.out.print(TicketSearchableFields.tags);
-					break;
-				}else if(userInput.trim().equals("quit")) {
+					System.out.println(" ");
+					System.out.println(" ");
+				}else if(userInput.trim().equals(MENU_QUIT)) {
 					break;
 				}else {
 					System.out.println("Invalid command valid options were enter to continue or quit to exit.\n");
-					break;
 				}			
-			}else if(userInput.trim().equals("quit")) {
-				//System.exit(1);
+			}else if(userInput.trim().equals(MENU_QUIT)) {
 				break;
 			}else {
 				System.out.println("Invalid command ! Please hit enter to continue or quit to exit.\n");
 			}
-		} while (!userInput.equals("quit"));
+		} while (!userInput.equals(MENU_QUIT));
 
 
 
